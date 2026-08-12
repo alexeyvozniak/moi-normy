58dc7afbcbcb351709eac7163bbd0865dc8b8d0b
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 15:58:29 +0300

    Bump service worker after entry action fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION='35.4.0';
+const APP_VERSION='35.4.1';
 const CACHE=`pravilo-shell-v${APP_VERSION}`;
 const CORE=[
   './','./index.html','./app.css','./app-core.js','./app-bootstrap.js','./manifest.webmanifest',
