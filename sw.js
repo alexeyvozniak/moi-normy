ecd654728fd9fb5941dfa776bea85344db2b8437
alexeyvozniak <62072958+alexeyvozniak@users.noreply.github.com>
2026-08-12 22:27:05 +0300

    Refresh offline cache for week and history polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION='35.12.0';
+const APP_VERSION='35.13.0';
 const CACHE=`pravilo-shell-v${APP_VERSION}`;
 const CORE=[
   './','./index.html','./app.css','./app-core.js','./app-bootstrap.js','./manifest.webmanifest',
